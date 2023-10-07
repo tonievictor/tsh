8f8a0b363ba2e7102c0f1e6d5472a5f54bcf6beb
Tonie Victor <victornwanochi@gmail.com>
2023-10-07 00:37:22 -0700

    Utility functions

---
+++ utils1.c
@@ -0,0 +1,143 @@
+#include "utilities.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @str: The string to convert.
+ *
+ * Return: The integer value of the string.
+ */
+
+int _atoi(const char *str)
+{
+	int i;
+	unsigned int num;
+
+	i = 0;
+	num = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+			return (1);
+		else if (str[i] >= '0' && str[i] <= '9')
+			num = (num * 10) + (str[i] - '0');
+		else
+			return (1);
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * _strncmp - Compare two strings up to a
+ * specified number of characters.
+ * @str1: The first string to be compared.
+ * @str2: The second string to be compared.
+ * @n: The maximum number of characters to compare.
+ *
+ * Return: 0 if the strings are equal up to n
+ * characters, an integer less
+ * than, or greater than zero if str1 is found,
+ * respectively, to be less
+ * than, to match, or be greater than str2.
+ */
+
+int _strncmp(const char *str1, const char *str2, size_t n)
+{
+
+	while (n > 0 && *str1 && *str2 && *str1 == *str2)
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * _strlen - Function to return the length of a string
+ * @s: String pointer to the string whose length is to be returned
+ * Return: Length of string
+ */
+int _strlen(const char *s)
+{
+	int lenstr = 0;
+
+	while (*s != '\0')
+	{
+		lenstr++;
+		s++;
+	}
+	return (lenstr);
+}
+
+/**
+ * _strdup - Duplicate a string.
+ * @src: The source string to duplicate.
+ *
+ * Description:
+ * This function duplicates a given null-terminated
+ * string @src and returns a
+ * pointer to a newly allocated string that contains a copy of @src.
+ *
+ * Return: A pointer to the duplicated string if successful,
+ * or NULL on failure.
+ *
+ * Note: The caller is responsible for freeing the
+ * memory allocated for the
+ * duplicated string when it is no longer needed.
+ */
+
+char *_strdup(char *src)
+{
+	char *dest;
+	size_t len;
+
+	if (src == NULL)
+	{
+		return (NULL);
+	}
+
+	len = strlen(src);
+
+	dest = (char *)malloc((len + 1) * sizeof(char));
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+
+	_strcpy(dest, src);
+
+	return (dest);
+}
+
+/**
+ * _stripstr - function to check if a user input is only just
+ * whitespaces
+ * @command: user input
+ *
+ * Return: 0 if a non-white space is encountered else 1
+ */
+
+int _stripstr(char *command)
+{
+	while (*command)
+	{
+		if (*command != ' ')
+		{
+			return (0);
+		}
+		command++;
+	}
+	return (1);
+}
+
---
+++ utils2.c
@@ -0,0 +1,112 @@
+#include "utilities.h"
+/**
+ * _strcpy - Copies a string pointed to
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int c = 0;
+
+	while (c >= 0)
+	{
+		*(dest + c) = *(src + c);
+		if (*(src + c) == '\0')
+			break;
+		c++;
+	}
+	return (dest);
+}
+
+/**
+ * _strcmp - Compare two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: An integer less than, equal to, or greater
+ * than zero if @s1 is found,
+ * respectively, to be less than, to match, or
+ * be greater than @s2.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int v;
+
+	v = 0;
+	while (s1[v] != '\0' && s2[v] != '\0')
+	{
+		if (s1[v] != s2[v])
+		{
+			return (s1[v] - s2[v]);
+		}
+		v++;
+	}
+	return (0);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: the pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c1 = 0, c2 = 0;
+
+	while (*(dest + c1) != '\0')
+	{
+		c1++;
+	}
+	while (c2 >= 0)
+	{
+		*(dest + c1) = *(src + c2);
+		if (*(src + c2) == '\0')
+			break;
+		c1++;
+		c2++;
+	}
+	return (dest);
+}
+
+/**
+ * count_elements - Count the number of elements
+ * in a null-terminated array.
+ * @array: The input array to count elements from.
+ *
+ * Return: The number of elements in the array.
+ */
+
+int count_elements(char **array)
+{
+	int count = 0;
+
+	while (array[count] != NULL)
+	{
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * remove_comments - checks fro the first occurence of # in command
+ * @command: user input to check
+ */
+
+void remove_comments(char *command)
+{
+	int i;
+
+	for (i = 0; command[i] != '\0'; i++)
+	{
+		if (command[i] == '#' && (!i || command[i - 1] == ' '))
+		{
+			command[i] = '\0';
+			break;
+		}
+	}
+}
+
