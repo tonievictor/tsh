b6c10a135b3721ae1c6cd6f61c88ce1986016bc4
Tonie Victor <victornwanochi@gmail.com>
2023-10-07 00:23:41 -0700

    Added support for the env builtin

---
+++ env_builtin.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "utilities.h"
+
+/**
+ * env_builtin - Execute the 'env' built-in command.
+ * @args: Array of command arguments.
+ * @argc: Number of arguments in the 'args' array.
+ * @command: The built-in command (unused in this function).
+ */
+
+void env_builtin(char **args, int argc, char *command)
+{
+	char **env = environ;
+	char *current;
+
+	(void)command;
+
+	if (argc > 1)
+	{
+		perror(args[0]);
+		return;
+	}
+
+	while (*env != NULL)
+	{
+		current = *env;
+		while (*current != '\0')
+		{
+			_putchar(*current);
+			current++;
+		}
+		_putchar('\n');
+		env++;
+	}
+}
+
